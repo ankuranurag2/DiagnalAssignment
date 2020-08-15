6c8d187801d980531e398948b3f88d579d3874ac
ankuranurag2 <ankuranurag2@gmail.com>
2020-08-15 12:05:37 +0530

    Initial Commit

---
+++ app/src/main/java/ankuranurag/diagnal/MainActivity.kt
@@ -0,0 +1,11 @@
+package ankuranurag.diagnal
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
