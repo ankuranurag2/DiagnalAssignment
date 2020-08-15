f5db0324c36f23ca28288f7553be72424b4e1bab
ankuranurag2 <ankuranurag2@gmail.com>
2020-08-15 14:49:28 +0530

    searchview and recyclerview added

--- app/src/main/java/ankuranurag/diagnal/MainActivity.kt
+++ app/src/main/java/ankuranurag/diagnal/MainActivity.kt
@@ -1,11 +1,45 @@
 package ankuranurag.diagnal
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.SearchView
+import androidx.databinding.DataBindingUtil
+import androidx.recyclerview.widget.RecyclerView
+import ankuranurag.diagnal.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var activityBinding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        activityBinding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.search_menu, menu)
+
+        val search = menu?.findItem(R.id.toolbar_search)
+        val searchView = (search?.actionView as? SearchView)
+        searchView?.setOnQueryTextListener(object : SearchView.OnQueryTextListener {
+            override fun onQueryTextSubmit(query: String?): Boolean {
+                searchView.clearFocus()
+                return false
+            }
+
+            override fun onQueryTextChange(newText: String?): Boolean {
+                if ((newText?.length ?: 0) > 3)
+                    handleSearch(newText!!)
+                return true
+            }
+        })
+
+        return true
+    }
+
+
+    private fun handleSearch(query: String) {
+
     }
 }
